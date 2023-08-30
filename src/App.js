3ab42a151c9b47f24965cfa66436b13a59d2fb79
CauaM10 <cauaclassroom30@gmail.com>
2023-08-30 16:40:13 -0300

    Finalizado

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "react";
+import "./App.css"
 
 function App() {
 
@@ -35,16 +36,18 @@ function App() {
   return (
       <>
       <hearder>
-          <h1>React DO</h1>
+          <h1 className="Titulo">To-Do List</h1>
       </hearder>
-        <div>
-          <input type="text" name="tarefa" placeholder="Digite sua tarefa" value={tarefa.texto} onChange={(e) => setTarefa({id: Math.random(), texto: e.target.value, status: false})}/>
-          <button onClick={addTarefa}>Adicionar</button>
+        <div className="Add-Escrever">
+          <div className="input-Escrever"><input maxLength={30} className="Escrever" type="text" name="tarefa" placeholder="Digite sua tarefa" value={tarefa.texto} onChange={(e) => setTarefa({id: Math.random(), texto: e.target.value, status: false})}/></div>
+          
+          <div className="butadd" ><button className="Addbutton" onClick={addTarefa}>Adicionar</button></div>
         </div>
-        <div>
-            <ul>
+        
+        <div className="caixa-tarefas">
+            <ul className="esppp">
               {listatarefas.map((item, index) => (
-                <li key={item.id}>{item.texto} <button onClick={ () => StatusTarefa(item.id, item.status)}>{item.status ?'Yes':'No'}</button> <button onClick={ () => ExcluirTarefa(item.id)}>Excluir</button></li>
+              <li className= {item.status ? 'Nfeito' : 'feito'} key={item.id}><div className="texto">{item.texto}</div> <div className="esp-butts"><button  className= {item.status ? 'but-nfeito' : 'but-feito'} onClick={ () => StatusTarefa(item.id, item.status)}>{item.status ? <i className="but-color-n" class="fa-solid fa-circle-check"></i> : <i className="check" class="fa-regular fa-circle-check"></i> }</button> <i onClick={ () => ExcluirTarefa(item.id)} class="fa-regular fa-trash-can"></i>  </div>  </li>
             ))}
             </ul>
         </div>
